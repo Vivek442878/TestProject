a75ca0b0f19a69d12241a01914561a6c44d0dc14
VivekKumarTiwari <vivektiwari.velociters@gmail.com>
2021-03-25 18:47:30 +0530

    commited by vivek

--- testProject/src/com/velociter/training/vivek/Point.java
+++ testProject/src/com/velociter/training/vivek/Point.java
@@ -10,34 +10,45 @@ public class Point
 	  public Point(double x, double y)
 	  
 	     {
+		
 		    this.x = x;
 		    this.y = y;
+		   
 		  }
 		 
 		  // Create a Point object from another Point
-		  public Point(Point p)
+		  public Point(final Point p)
 		  {
+			
+	
 		    x = p.x;
 		    y = p.y;
+		  
+		   
 		  }
 		 
 		  // Get the value of the x coordinate
 		  double getX()
 		  {
+			
 		    return x;
 		  }
 		 
 		  // Get the value of the y coordinate
 		  double getY()
 		  {
+			 
 		    return y;
 		  }
 		 
-		  public String toString(){
+		  public String toString()
+		  {
+			  
 		    return x +","+y;
+		    //System.out.println("vivek");
 		  }
 		 
-		  
+		 
 		}
 
 
