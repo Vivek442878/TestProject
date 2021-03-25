3272ff7c3d8cf44ff6a40c3c9e674e6bd00da82e
VivekKumarTiwari <vivektiwari.velociters@gmail.com>
2021-03-25 18:34:56 +0530

    commited by vivek

---
+++ testProject/src/com/velociter/training/vivek/Point.java
@@ -0,0 +1,43 @@
+package com.velociter.training.vivek;
+
+public class Point
+
+{
+	
+	// Fields store the point coordinates
+	  private double x;
+	  private double y;
+	  public Point(double x, double y)
+	  
+	     {
+		    this.x = x;
+		    this.y = y;
+		  }
+		 
+		  // Create a Point object from another Point
+		  public Point(Point p)
+		  {
+		    x = p.x;
+		    y = p.y;
+		  }
+		 
+		  // Get the value of the x coordinate
+		  double getX()
+		  {
+		    return x;
+		  }
+		 
+		  // Get the value of the y coordinate
+		  double getY()
+		  {
+		    return y;
+		  }
+		 
+		  public String toString(){
+		    return x +","+y;
+		  }
+		 
+		  
+		}
+
+
