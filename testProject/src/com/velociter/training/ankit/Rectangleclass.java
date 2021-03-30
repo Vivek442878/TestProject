9fe1abe17d47b9ed1cc8164a03a2d4f76641016e
Ankit <ankit.velociters@gmail.com>
2021-03-30 10:53:55 +0530

    Carpet.java added by ankit

--- testProject/src/com/velociter/training/ankit/Rectangleclass.java
+++ testProject/src/com/velociter/training/ankit/Rectangleclass.java
@@ -1,11 +1,11 @@
 package com.velociter.training.ankit;
 
-class Point {
+ class Point {
   //Points for coordinate  store the point coordinates from rectangle
 	  private double x;
 	  private double y;
-	//First Constructor get new coordinates
-  public Point(double x, double y) {
+   //First Constructor get new coordinates
+   public Point(double x, double y) {
     this.x = x;
     this.y = y;
   }
@@ -38,20 +38,18 @@ class Point {
   private Point bottomRight;
  
  public Rectangle(double x1, double y1, double x2, double y2) {
-    
-    
     topLeft = new Point(Math.min(x1,x2),Math.min(y1,y2)); //topleft uses the smaller of x1,x2 and y1,y2
-    bottomRight = new Point(Math.max(x1,x2),Math.max(y1,y2)); //topright uses the larger of x1,x2 and y1,y2
+    bottomRight = new Point(Math.max(x1,x2),Math.max(y1,y2)); //bottomright uses the larger value x1,x2 and y1,y2
   }
- 
-  public Rectangle(Point topL, Point bottomR) {
-    this(topL.getX(), topL.getY(), bottomR.getX(), bottomR.getY());
-  }
- 
+
   public Rectangle(Rectangle rect) {
     topLeft     = new Point(rect.topLeft);
     bottomRight = new Point(rect.bottomRight);
   }
+  
+  public Rectangle(Point topL, Point bottomR) {
+	    this(topL.getX(), topL.getY(), bottomR.getX(), bottomR.getY());
+	  }
  
   // Methods to create a rectangleEnClosing the current rectangle and the argument
   public Rectangle encloses(Rectangle rect) {
@@ -61,35 +59,33 @@ class Point {
  }
  
   public String toString()  {
-    return "Rectangle: " + topLeft + " : " + bottomRight;
+    return "" + topLeft + " : " + bottomRight;
   }
 }
 
-class Rectangleclass
+ public class Rectangleclass
 	{
 	  
     	  public static void main(String args[]) 
 		  {
-	      Rectangle[] rectangle = new Rectangle[1];
+	      Rectangle[] rectangle = new Rectangle[4];
 	      Rectangle enClosing;
 	 
 	   
 	    for(int i = 0 ; i < rectangle.length; ++i) {
 	      rectangle[i] = new Rectangle(Math.random()*100, Math.random()*100, Math.random()*100, Math.random()*100);
 	      System.out.print("Coords of rectangle " + i + " are: ");
-	      System.out.println(rectangle[i]);
+	     // System.out.println(rectangle[i].toString());
+	      System.out.println(rectangle[i]+"\n");
 	    }
-	 
-	    
-	   enClosing = rectangle[0];
-	 
-	   
+	  
+	    enClosing = rectangle[0];
 	    for(Rectangle rect : rectangle) //result  
 	    {
 	     enClosing =enClosing.encloses(rect);
 	    }
 	 
-	    System.out.println("Coords of enClosing rectangle are ");
+	    System.out.print("Coords of enClosing rectangle are ");
 	    System.out.println(enClosing);
 	  }
 	}
